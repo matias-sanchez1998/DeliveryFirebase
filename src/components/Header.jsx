9e8e6264264962bcb97461d20e1f20cd2f03ac26
magomatiassss <magomatiassss@gmail.com>
2022-12-14 20:19:59 -0300

    add github

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -111,7 +111,7 @@ const Header = () => {
                 exit={{ opacity: 0, scale: 0.6 }}
                 className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
               >
-                {user && user.email === 'sanchezmatias419@gmail.com' && (
+                {user && user.email === 'sanchezmatias419@gmail.comn' && (
                   <Link to={"/createItem"}>
                     <p
                       className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
@@ -152,7 +152,7 @@ const Header = () => {
 
         <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo" />
-          <p className="text-headingColor text-xl font-bold"> Delivery</p>
+          <p className="text-headingColor text-xl font-bold"> DeliveryMan</p>
         </Link>
 
         <div className="relative">
