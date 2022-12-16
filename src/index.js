d2ac894817b3f7458a5fb788b8d88763df031b95
magomatiassss <magomatiassss@gmail.com>
2022-12-16 14:31:24 -0300

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import "./index.css";
-
 import { HashRouter as Router } from "react-router-dom";
 
 import App from "./App";
