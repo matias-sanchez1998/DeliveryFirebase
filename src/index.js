af39b2f2db962b6f1fb1f4fe7ebe2efb58ccb088
magomatiassss <magomatiassss@gmail.com>
2022-12-16 16:39:13 -0300

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import { HashRouter as Router } from "react-router-dom";
 
@@ -8,16 +8,16 @@ import { StateProvider } from "./context/StateProvider";
 import { initialState } from "./context/initialState";
 import reducer from "./context/reducer";
 
-const container = document.getElementById('root');
-const root = createRoot(container); 
-root.render(
-  <div>Hola</div>
-  );
-// root.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
+{
+  /* // root.render(
 //   <Router >
 //     <StateProvider initialState={initialState} reducer={reducer}>
 
 //     <App />
 
 //     </StateProvider>
-//   </Router>);
+//   </Router>); */
+}
